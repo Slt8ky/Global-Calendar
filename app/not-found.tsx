9abf209bfeec138f17a18068caba492b60fd6ade
Slt8ky <Slt8ky@gmail.com>
2026-07-09 16:48:37 +0800

    On branch main Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	new file:   app/not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function Page() {
+    return (
+        <div className="w-full h-dvh flex justify-center items-center">
+            <Card>
+                <CardContent>
+                    Page Not Found
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
