c3fdaab2b1866f8f7693218b16cc8ff6b5404c5d
Slt8ky <Slt8ky@gmail.com>
2026-07-09 16:07:37 +0800

    On branch main Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   lib/utils.ts
    no changes added to commit (use "git add" and/or "git commit -a")

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { EventColor, EventData } from "./types";
-import { RefObject } from "react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -12,8 +10,4 @@ export function convertDate(dateString: string) {
   const date = new Date();
   date.setFullYear(y, m, d);
   return date;
-}
-
-export function normalizeEvents(events: EventData[], colorsRef: RefObject<EventColor | null>) {
-  return events.map(event => Object.fromEntries(Object.entries(event).filter(([k]) => Object.keys(['id', 'start', 'end', 'summary'].includes(k))))) as EventData[];
 }
